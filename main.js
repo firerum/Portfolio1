c9797604370508e04dc052c2ca66a1af6c98d707
Ademuyiwa Adewuyi <ademuyiwaadewuyi@gmail.com>
2020-11-15 22:23:30 +0100

    added some new info to the site. I also added carousel

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+/* Slider function */
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector("#next");
+const prevBtn = document.querySelector("#prev");
 
+function nextImage() {
+   // Get current image
+   const currentImage = document.querySelector(".current");
+   // Remove current class
+   currentImage.classList.remove("current");
+   // Add current to the next Image to make it visible
+   if(currentImage.nextElementSibling) {
+      currentImage.nextElementSibling.classList.add("current");
+      // Add current back to the first image if there are no more images
+   } else {
+      slides[0].classList.add("current");
+   }
+}
+
+function previousImage() {
+   // Get current image
+   const currentImage = document.querySelector(".current");
+   // Remove current class
+   currentImage.classList.remove("current");
+   // Add current to the next Image to make it visible
+   if(currentImage.previousElementSibling) {
+      currentImage.previousElementSibling.classList.add("current");
+      // Add current back to the last image if there are no more images
+   } else {
+      slides[slides.length - 1].classList.add("current");
+   }
+}
+
+nextBtn.addEventListener("click", nextImage);
+prevBtn.addEventListener("click", previousImage);
