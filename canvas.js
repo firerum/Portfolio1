095c49d2f279c3dad23bc09945926714b7fef980
Ademuyiwa Adewuyi <ademuyiwaadewuyi@gmail.com>
2020-11-26 14:17:18 +0100

    I added mouse interactivity to the mouse canvas, and  made the colors random

--- canvas.js
+++ canvas.js
@@ -2,6 +2,23 @@ const canvas = document.getElementById("canvas");
 const banner = document.getElementById("header-canvas");
 const ctx = canvas.getContext("2d");
 
+
+// Give the circles random colors
+let colorArray = ["skyblue", "#f9f871", "#57DFC2", "#d65db1"];
+
+// Create a mouse object to detect the mouse position
+let mouse = {
+   x: undefined,
+   y: undefined
+};
+
+// Add mouse event to the window object
+window.addEventListener("mousemove", function(e) {
+   // initialize the mouse object by giving ot the current mouse position
+   mouse.x = e.x;
+   mouse.y = e.y;
+});
+
 // Write constructor Object to create many circles;
 function Circle(x, y, dx, dy, radius) {
    this.x = x;
@@ -9,14 +26,16 @@ function Circle(x, y, dx, dy, radius) {
    this.dx = dx;
    this.dy = dy;
    this.radius = radius;
+   this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 }
 
 // create function to draw the circle on canvas
 Circle.prototype.draw = function() {
    ctx.beginPath();
    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-   ctx.strokeStyle = "rgba(0, 255, 255, .3)";
+   ctx.strokeStyle = this.color;
    ctx.stroke();
+   ctx.fill();
 };
 
 // Update the circle to enable movement on each repaint of the canvas
@@ -31,6 +50,17 @@ Circle.prototype.update = function() {
 
    this.x += this.dx;
    this.y += this.dy;
+   // Create Mouse interactivity by judging position between mouse and circle.
+   if(mouse.x - this.x < 50 && mouse.x - this.x > -50 &&
+      mouse.y -this.y < 50 && mouse.y -this.y > -50) {
+         // Create maximum size for the circle growth
+         if(this.radius > 40) {
+            this.radius -= 1;
+         }
+         this.radius += 1;
+   } else if(this.radius > 10) {
+      this.radius -= 1;
+   }
 };
 
 let circleArray = [];
@@ -45,6 +75,15 @@ for (let i = 0; i < 100; i++) {
    // Push the new object circles into the array.
    circleArray.push(new Circle(x, y, dx, dy, radius));
 }
+
+// Redestribute circles on window resize
+window.addEventListener("resize", function() {
+   canvas.width = banner.offsetWidth;
+   canvas.height = banner.offsetHeight;
+   for(let i = 0; i < circleArray.length; i++) {
+      circleArray[i].draw();
+   }
+});
 
 function animate() {
    ctx.clearRect(0, 0, banner.offsetWidth, banner.offsetHeight);
